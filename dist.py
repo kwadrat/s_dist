5f7ee18c43ebe4638b43dd483c85be55b78c9f1a
Piotr Kasprzyk <ciri@ciri.pl>
2021-07-11 15:21:14 +0200

    Add Skrzynka

---
+++ dist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+'''
+Template file for testing functions and classes
+./simple_tdd_template.py test; red_green_bar.py $? $COLUMNS
+'''
+
+import sys
+import unittest
+
+class Skrzynka:
+    pass
+
+class TestDist(unittest.TestCase):
+    def test_something(self):
+        '''
+        TestDist:
+        '''
+        obk = Skrzynka()
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'test':
+        unittest.main(argv=sys.argv[:1])
